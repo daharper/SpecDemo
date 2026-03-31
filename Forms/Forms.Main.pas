d9a454bd57ad9c2196200ffa2a15598e40666ba6
daharper <daharper@hotmail.com>
2026-03-31 22:16:24 +0100

    minor name change

--- Forms/Forms.Main.pas
+++ Forms/Forms.Main.pas
@@ -46,25 +46,24 @@ procedure TMainForm.FormShow(Sender: TObject);
 var
   scope: TScope;
 begin
-  var orderReport := TOrderReport.Create;
-
+  var report  := TOrderReport.Create;
+  var section := report.AsDynamic;
   var service := scope.Owns(TOrderService.Create);
-  var report  := orderReport.AsDynamic;
 
-  report.HighValueOrders                        := service.GetHighValueOrders;
-  report.InternationalOrders                    := service.GetInternationalOrders;
+  section.HighValueOrders                        := service.GetHighValueOrders;
+  section.InternationalOrders                    := service.GetInternationalOrders;
 
-  report.ConfirmedHighValueOrders               := service.GetConfirmedHighValueOrders;
-  report.PriorityInternationalOrders            := service.GetPriorityInternationalOrders;
-  report.UnconfirmedHighRiskInternationalOrders := service.GetHighRiskUnconfirmedInternationalOrders;
-  report.OrdersNeedingSpecialHandling           := service.GetOrdersNeedingSpecialHandling;
+  section.ConfirmedHighValueOrders               := service.GetConfirmedHighValueOrders;
+  section.PriorityInternationalOrders            := service.GetPriorityInternationalOrders;
+  section.UnconfirmedHighRiskInternationalOrders := service.GetHighRiskUnconfirmedInternationalOrders;
+  section.OrdersNeedingSpecialHandling           := service.GetOrdersNeedingSpecialHandling;
 
-  report.OrdersReadyForPriorityFulfillment      := service.GetOrdersReadyForPriorityFulfillment;
-  report.PremiumInternationalOrders	            := service.GetPremiumInternationalOrders;
-  report.TrustedSpecialHandlingOrders           := service.GetTrustedSpecialHandlingOrders;
-  report.OrdersForEscalatedReview               := service.GetOrdersForEscalatedReview;
+  section.OrdersReadyForPriorityFulfillment      := service.GetOrdersReadyForPriorityFulfillment;
+  section.PremiumInternationalOrders	           := service.GetPremiumInternationalOrders;
+  section.TrustedSpecialHandlingOrders           := service.GetTrustedSpecialHandlingOrders;
+  section.OrdersForEscalatedReview               := service.GetOrdersForEscalatedReview;
 
-  fHtmlPath := TOrdersHtmlRenderer.Execute(orderReport);
+  fHtmlPath := TOrdersHtmlRenderer.Execute(report);
 
   Browser.CreateWebView;
 end;
